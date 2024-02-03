79f20046580a180821e8016613f4e6ac9a443869
Adilet <adletbek043@gmail.com>
2024-02-03 16:09:40 +0600

    initial commit

---
+++ src/Student.java
@@ -0,0 +1,34 @@
+public class Student extends Person {
+    private double gpa;
+
+    public Student() {
+        super();
+    }
+
+    public Student(String name, String surname, double gpa) {
+        super(name, surname);
+        this.gpa = gpa;
+    }
+
+    public String toString() {
+        return String.format("Student: %s earns %.2f tenge", super.toString(), getPaymentAmount());
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    @Override
+    public double getPaymentAmount() {
+        return (gpa > 2.67) ? 36660.00 : 0.00;
+    }
+
+    @Override
+    public String getPosition() {
+        return "Student";
+    }
+}
